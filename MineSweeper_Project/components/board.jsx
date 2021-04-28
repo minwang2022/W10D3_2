e7c0e4757a24d6a7f62d7c6f856d85e464035dfd
Robby Balistreri <rbalistreri5@gmail.com>
2021-04-28 14:26:56 -0500

    Tile work

--- MineSweeper_Project/components/board.jsx
+++ MineSweeper_Project/components/board.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import * as Minesweeper from "../minesweeper.js"
+import Tile from "./tile"
 
 class Board extends React.Component {
     constructor(props) {
         debugger
         super(props)
+        this.rowCreator = this.rowCreator.bind(this)
+    }
+
+    rowCreator() {
+        const newBoard = this.props.board;
+        return (newBoard.grid.map( (row, idx) => {
+
+            return (<div key={idx}>{row.map((tile, idx2) => {
+               return (< Tile key ={idx2} tileObj={tile} updateGame={this.props.updateGame} />)
+            }
+            )}
+            </div>)
+         }))
     }
 
     render  () {
+        debugger;
         return (
-            <div>{JSON.stringify(this.props)}</div>
+            <div>
+                {this.rowCreator()}
+            </div>
         )
     }
 
--- MineSweeper_Project/components/tile.jsx
+++ MineSweeper_Project/components/tile.jsx
@@ -4,11 +4,29 @@ import * as Minesweeper from "../minesweeper.js"
 class Tile extends React.Component {
     constructor(props) {
         super(props)
+        this.tileValue = this.tileValue.bind(this)
+    }
+
+    tileValue() {
+        const currTile = this.props.tileObj;
+        let tileClass;
+        if (currTile.explored) {
+            if (currTile.bombed) {
+                return ['💣', 'bombed tile'];
+            } else {
+                return [currTile.adjacentBombCount(), 'revealed tile'];
+            }
+        } else if (currTile.flagged) {
+            return ['⚑', 'flagged tile']  ;
+        } else {
+            return ['_', 'empty tile'];
+        }
     }
 
     render  () {
+        let value = this.tileValue();
         return (
-            <div>{this.props}</div>
+            <div className={value[1]}>{value[0]}</div>
         )
     }
 
